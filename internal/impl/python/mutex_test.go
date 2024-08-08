e9adcda89465266a56625cf96d4408932b16d1fd
Dave Voutila <voutilad@gmail.com>
2024-08-08 18:06:23 -0400

    Add quick unit test for lock acquisition timeouts.

--- internal/impl/python/mutex_test.go
+++ internal/impl/python/mutex_test.go
@@ -37,3 +37,34 @@ func TestMutexAcquisitionCanBeCancelled(t *testing.T) {
 		t.Fatal("Timeout waiting for cancellation.")
 	}
 }
+
+func TestMutexAcquisitionCanTimeout(t *testing.T) {
+	m := NewContextAwareMutex()
+	ctx := context.Background()
+
+	// First, we lock.
+	_ = m.Lock()
+
+	// Then we try locking again, but we _should_ time out.
+	ctx2, _ := context.WithTimeout(ctx, 200*time.Millisecond)
+	timedOut := make(chan bool)
+
+	go func() {
+		err := m.LockWithContext(ctx2)
+		if err != nil {
+			timedOut <- true
+		} else {
+			timedOut <- false
+		}
+	}()
+
+	// Cancel our locking.
+	select {
+	case r := <-timedOut:
+		if r == false {
+			t.Fatal("Mutex locking was not cancelled.")
+		}
+	case <-time.After(1 * time.Second):
+		t.Fatal("Timeout waiting for cancellation.")
+	}
+}
